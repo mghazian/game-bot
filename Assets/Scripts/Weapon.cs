580405d3a58db278c689510ffc712ba1bd156def
rafiar <rafiar254@gmail.com>
2017-11-28 21:27:23 +0700

    Weapon Script

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon
+{
+	private uint damage;
+
+	public Weapon (uint damage)
+	{
+		this.damage = damage;
+	}
+
+	abstract void fire ();
+}
+
