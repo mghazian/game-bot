f03845203b9ac6212d5b1b057532aed6686fb443
rafiar <rafiar254@gmail.com>
2017-11-28 21:26:04 +0700

    Mele Weapon Script

---
+++ Assets/Scripts/MeleeWeapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeWeapon
+{
+	public MeleeWeapon (uint damage) : base (damage)
+	{
+		// this is sparta
+	}
+
+	public void fire ()
+	{
+		// fuck
+	}
+}
+
