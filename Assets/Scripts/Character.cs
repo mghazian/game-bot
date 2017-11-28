80668b4d584cacca0c7b28f8f8bad0ece3547e96
rafiar <rafiar254@gmail.com>
2017-11-28 21:25:15 +0700

    Character Script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character {
+
+	private uint id { get; }
+	private uint health { get; set; }
+
+	private uint scalarSpeed { get; set; }
+	private uint jumpHeight { get; set; }
+
+	// More personalization e.g. skins etc.
+	// ...
+
+	// Weapons
+	// ...
+
+	// State
+	// ...
+
+	public Character (uint id, uint scalarSpeed, uint jumpHeight)
+	{
+		this.id = id;
+		this.scalarSpeed = scalarSpeed;
+		this.jumpHeight = jumpHeight;
+	}
+}
