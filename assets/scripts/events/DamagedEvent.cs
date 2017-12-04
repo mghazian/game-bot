7551e9f4271202371015f44e538abb04b943961b
mghazian <muhghazian@gmail.com>
2017-12-04 23:20:42 +0700

    Created event for character receive damage

---
+++ assets/scripts/events/DamagedEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DamagedEvent : UnityEvent <int>
+{
+	//	TODO: Change 'object' to its proper class
+	private object parentCharacter;
+
+	public DamagedEvent (object character)
+	{
+		parentCharacter = character;
+		this.AddListener ( DamageMe );
+	}
+
+	private void DamageMe (int value)
+	{
+		/*
+		if ( parentCharacter != null )
+			parentCharacter.health -= value;
+		*/
+	}
+}
