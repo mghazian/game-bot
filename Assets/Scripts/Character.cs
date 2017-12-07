617c03d731b493c5e9351f6c6146f67d59544ec6
wildanlsfs <wildanlsfs@gmail.com>
2017-12-07 23:02:42 +0700

    adding movement on player

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character {
+public class Character : CharacterContainer {
 
 	private uint id { get; }
 	private uint health { get; set; }
@@ -10,19 +10,82 @@ public class Character {
 	private uint scalarSpeed { get; set; }
 	private uint jumpHeight { get; set; }
 
+	private Rigidbody2D playerRigidBody2D;
+
+	private bool isJump = false;
+	private bool isRanged = true;
 	// More personalization e.g. skins etc.
 	// ...
 
 	// Weapons
 	// ...
+	void Start()
+	{
+		playerRigidBody2D = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
 
-	// State
-	// ...
+	void Update()
+	{
+		//control movement
+		if(Input.GetKeyDown(KeyCode.UpArrow) && !isJump)
+		{
+			Jump ();
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			Move("Left");
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			Move("Right");
+		}	
+	}
 
 	public Character (uint id, uint scalarSpeed, uint jumpHeight)
 	{
 		this.id = id;
 		this.scalarSpeed = scalarSpeed;
 		this.jumpHeight = jumpHeight;
+	}
+
+	// State
+
+	private void Move(string direction)
+	{
+		if (direction == "Left") 
+		{
+			playerRigidBody2D.velocity = new Vector3 (-3.0f , playerRigidBody2D.velocity.y, 0.0f);
+		} 
+
+		else 
+		{
+			playerRigidBody2D.velocity = new Vector3 (3.0f, playerRigidBody2D.velocity.y, 0.0f);
+
+		}
+	}
+
+	private void Jump()
+	{
+		isJump = true;
+		playerRigidBody2D.velocity = new Vector3 (playerRigidBody2D.velocity.x, 8f,0.0f);
+	}
+
+	private void Attack()
+	{
+
+	}
+
+	private void ChangeWeapon()
+	{
+
+	}
+
+	void OnCollisionStay2D (Collision2D anotherObject)
+	{
+		if (anotherObject.collider.tag == "Ground") {
+			isJump = false;
+		}
 	}
 }
