6c7dff95858a1f89b719822252d5062f08c1f4a6
Rafiar Rahmansyah <rafiar254@gmail.com>
2017-12-18 02:45:13 +0700

    updated team class

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -23,6 +23,19 @@ public class Team : MonoBehaviour {
         teamName = name;
     }
 
+    // compare if the team name is same with a certain string of name
+    public bool teamNameCompare(string name)
+    {
+        if (string.Compare(teamName,name) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     // add a certain Character Object to a certain Team Object
     public void addCharacter(Character objectPointer)
     {
