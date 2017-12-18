4a1345222c2c869a5ba9d3fec61b636c61ada252
Rina Wijaya <rinawkw@gmail.com>
2017-12-18 18:23:31 +0700

    button system modify

---
+++ Assets/Scripts/SetCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetCanvas : IClickable {
+
+    private Image activeImage;
+    private string imagePath;
+
+    public SetCanvas(Image active_image, string image_path)
+    {
+        activeImage = active_image;
+        imagePath = image_path;
+    }
+
+    public void Down()
+    {
+        activeImage.sprite = Resources.Load<Sprite>(imagePath);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SetCanvasButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetCanvasButton : MyButton {
+    [SerializeField]
+    public Image activeImage;
+    public string imagePath;
+
+    // Use this for initialization
+    void Start () {
+        iclickable = new SetCanvas(activeImage,imagePath);
+        Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
