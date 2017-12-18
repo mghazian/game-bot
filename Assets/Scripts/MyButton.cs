e55243500931e9ad9c20a2e3142af683117ca373
Rina Wijaya <rinawkw@gmail.com>
2017-12-18 11:59:58 +0700

    Button strategy pattern

---
+++ Assets/Scripts/MyButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MyButton : Button {
+
+
+    protected IClickable iclickable;
+
+    public MyButton()
+    {
+        this.onClick.AddListener(Down);
+    }
+    public void Down()
+    {
+        iclickable.Down();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
